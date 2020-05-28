288cbcba171036c033f24aa866db0cc63e72aa5d
Daniel Lee <djklee@edu.uwaterloo.ca>
2020-05-28 18:38:21 -0400

    Updates

---
+++ precache-manifest.89804c50054d91e5de20148bbbcf0174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcfa343eb5372e617753690e9c7f1721",
+    "url": "/perfect-tft/index.html"
+  },
+  {
+    "revision": "5b42b8a8fcb462417f30",
+    "url": "/perfect-tft/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9b3af573ad55963e3a33",
+    "url": "/perfect-tft/static/css/main.c554460a.chunk.css"
+  },
+  {
+    "revision": "5b42b8a8fcb462417f30",
+    "url": "/perfect-tft/static/js/2.3319f256.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/perfect-tft/static/js/2.3319f256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3af573ad55963e3a33",
+    "url": "/perfect-tft/static/js/main.cce94ebb.chunk.js"
+  },
+  {
+    "revision": "277a2fc10b62ec47ec2e",
+    "url": "/perfect-tft/static/js/runtime-main.a1ea675b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a41a903326eabad4def64e6d461b9ef.js"
+  "/perfect-tft/precache-manifest.89804c50054d91e5de20148bbbcf0174.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/perfect-tft/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
