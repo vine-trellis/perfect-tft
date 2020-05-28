9da7bcbb1586d99d370c3eb928ce40a319fcece2
Daniel Lee <djklee@edu.uwaterloo.ca>
2020-05-28 18:22:44 -0400

    Updates

---
+++ precache-manifest.f6e7f4bc8f1033895f3bec6adfb4b7ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12012a6d60c9009c6e5fdac797b40bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b42b8a8fcb462417f30",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7513ae91ff81b720ac1c",
+    "url": "/static/css/main.c554460a.chunk.css"
+  },
+  {
+    "revision": "5b42b8a8fcb462417f30",
+    "url": "/static/js/2.3319f256.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3319f256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7513ae91ff81b720ac1c",
+    "url": "/static/js/main.228e3011.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
